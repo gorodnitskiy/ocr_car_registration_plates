72668baede082cc9a65ac52c6d15702d35e2c935
Alex Gorodnitskiy <alekc1272@gmail.com>
2021-03-16 14:11:23 +0300

    (+) add crnn model py

---
+++ crnn_model.py
@@ -0,0 +1,134 @@
+import numpy as np
+
+import torch
+from torch.nn import Module, Sequential, Conv2d, AvgPool2d, GRU, Linear
+from torchvision import models
+
+
+def pred_to_string(pred, abc):
+    seq = []
+    for i in range(len(pred)):
+        label = np.argmax(pred[i])
+        seq.append(label - 1)
+    out = []
+    for i in range(len(seq)):
+        if len(out) == 0:
+            if seq[i] != -1:
+                out.append(seq[i])
+        else:
+            if seq[i] != -1 and seq[i] != seq[i - 1]:
+                out.append(seq[i])
+    out = ''.join([abc[c] for c in out])
+    return out
+
+
+def decode(pred, abc):
+    pred = pred.permute(1, 0, 2).cpu().data.numpy()
+    outputs = []
+    for i in range(len(pred)):
+        outputs.append(pred_to_string(pred[i], abc))
+    return outputs
+
+
+class FeatureExtractor(Module):
+    def __init__(self, input_size=(64, 320), output_len=20):
+        super().__init__()
+
+        h, w = input_size
+        resnet = getattr(models, 'resnet18')(pretrained=True)
+        self.cnn = Sequential(*list(resnet.children())[:-2])
+
+        self.pool = AvgPool2d(kernel_size=(h // 32, 1))
+        self.proj = Conv2d(w // 32, output_len, kernel_size=1)
+
+        self.num_output_features = self.cnn[-1][-1].bn2.num_features
+
+    def apply_projection(self, x):
+        """Use convolution to increase width of a features.
+        Accepts tensor of features (shaped B x C x H x W).
+        Returns new tensor of features (shaped B x C x H x W').
+        """
+        x = x.permute(0, 3, 2, 1).contiguous()
+        x = self.proj(x)
+        x = x.permute(0, 2, 3, 1).contiguous()
+        return x
+
+    def forward(self, x):
+        # Apply conv layers
+        features = self.cnn(x)
+
+        # Pool to make height == 1
+        features = self.pool(features)
+
+        # Apply projection to increase width
+        features = self.apply_projection(features)
+
+        return features
+
+
+class SequencePredictor(Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes, dropout=0.3, bidirectional=False):
+        super(SequencePredictor, self).__init__()
+
+        self.num_classes = num_classes
+        self.rnn = GRU(input_size=input_size,
+                       hidden_size=hidden_size,
+                       num_layers=num_layers,
+                       dropout=dropout,
+                       bidirectional=bidirectional)
+
+        fc_in = hidden_size if not bidirectional else 2 * hidden_size
+        self.fc = Linear(in_features=fc_in,
+                         out_features=num_classes)
+
+    def _init_hidden_(self, batch_size):
+        """Initialize new tensor of zeroes for RNN hidden state.
+        Accepts batch size.
+        Returns tensor of zeros shaped (num_layers * num_directions, batch, hidden_size).
+        """
+        num_directions = 2 if self.rnn.bidirectional else 1
+        return torch.zeros(self.rnn.num_layers * num_directions, batch_size, self.rnn.hidden_size)
+
+    def _prepare_features_(self, x):
+        """Change dimensions of x to fit RNN expected input.
+        Accepts tensor x shaped (B x (C=1) x H x W).
+        Returns new tensor shaped (W x B x H).
+        """
+        x = x.squeeze(1)
+        x = x.permute(2, 0, 1)
+        return x
+
+    def forward(self, x):
+        x = self._prepare_features_(x)
+
+        batch_size = x.size(1)
+        h_0 = self._init_hidden_(batch_size)
+        h_0 = h_0.to(x.device)
+        x, h = self.rnn(x, h_0)
+
+        x = self.fc(x)
+        return x
+
+
+class CRNN(Module):
+    def __init__(self, alphabet,
+                 cnn_input_size=(64, 320), cnn_output_len=20,
+                 rnn_hidden_size=128, rnn_num_layers=2,
+                 rnn_dropout=0.3, rnn_bidirectional=False):
+        super().__init__()
+        self.alphabet = alphabet
+        self.features_extractor = FeatureExtractor(
+            input_size=cnn_input_size,
+            output_len=cnn_output_len)
+        self.sequence_predictor = SequencePredictor(
+            input_size=self.features_extractor.num_output_features,
+            hidden_size=rnn_hidden_size,
+            num_layers=rnn_num_layers,
+            num_classes=len(alphabet) + 1,
+            dropout=rnn_dropout,
+            bidirectional=rnn_bidirectional)
+
+    def forward(self, x):
+        features = self.features_extractor(x)
+        sequence = self.sequence_predictor(features)
+        return sequence
